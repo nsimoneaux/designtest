e454a790a30cdd800d3bcfe81101b3d099488136
nsimoneaux <nsimoneaux@gmail.com>
2015-11-06 00:40:32 -0700

    cleanup

--- js/grayscale.js
+++ js/grayscale.js
@@ -62,7 +62,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(40.552767, -105.066667);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
